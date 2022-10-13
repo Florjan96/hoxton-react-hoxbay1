b6fa1dee378e7989938507569338b8e9e0553466
Florjan96 <florianshiqerukaj@yahoo.com>
2022-10-13 23:34:18 +0200

    Little change

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react"
+import { getRandomColor } from "../helpers"
+
 export function Categories(){
+const[categories,setCategories]=useState([])
+useEffect(()=>{
+    fetch('http://localhost:4000/categories')
+    .then(resp=>resp.json())
+    .then(categoriesFromServer=>setCategories(categoriesFromServer))
+},[])
+
+
     return(
-        <h1>Kategoritee</h1>
+        <section className="categories-container main-wrapper">
+    <ul className="categories-container__list">
+        {categories.map(category=>(
+ <li style={{backgroundColor:getRandomColor()}}>
+ <a href={`/categories/${category.id}`}>{category.name}</a>
+</li>
+        ))}
+     
+     
+    </ul>
+  </section>
     )
 }
