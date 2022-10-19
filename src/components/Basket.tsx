de29bbd6510e5aba055a7e8139b7c91766156b62
Florjan96 <florianshiqerukaj@yahoo.com>
2022-10-19 19:45:19 +0200

    Working at basket section.We can chose quantity and calculate the total

--- src/components/Basket.tsx
+++ src/components/Basket.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { isHtmlElement } from "react-router-dom/dist/dom"
 
 export function Basket(){
 const[basket,setBasket]=useState([])
@@ -9,33 +10,50 @@ useEffect(()=>{
   .then(basketFromServer=>setBasket(basketFromServer))
 },[])
 
+let total=0
+for( let item of basket){
+  total += item.quantity * item.product.price 
+}
+
     return(
         <section className="basket-container">
         <h2>Your Basket</h2>
         <ul>
-          <li>
-            <article className="basket-container__item">
-              <img
-                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-                alt="Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
-                width="90"
-              />
-              <p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
-              <p>
-                Qty:
-                <select
-                ><option value="0">0</option
-                  ><option value="1">1</option
-                  ><option value="2">2</option
-                  ><option value="3">3</option>
-                  </select>
-            
-              </p>
-              <p>Item total: £109.95</p>
-            </article>
-          </li>
+          {basket.map((basketProduct)=>(
+  <li>
+  <article className="basket-container__item">
+    <img
+      src={basketProduct.product.image}
+      alt="Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
+      width="90"
+    />
+    <p>{basketProduct.product.title}</p>
+    <p>
+      Qty:
+      <select value={basketProduct.quantity}
+      onChange={(event)=>{
+console.log(event.target.value)
+const basketCopy=structuredClone(basket)
+
+const match=basketCopy.find(target=>target.id ===basketProduct.id  )
+match.quantity=Number(event.target.value)
+setBasket(basketCopy)
+      }}
+      >
+        <option value="0">0</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        </select>
+  
+    </p>
+    <p>${basketProduct.product.price * basketProduct.quantity}</p>
+  </article>
+</li>
+          ))}
+        
         </ul>
-        <h3>Your total: £109.95</h3>
+        <h3>${total}</h3>
       </section>
     )
 }
